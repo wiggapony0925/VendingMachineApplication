8f58d27648db23d7b5c4aa755891696c45365fce
wiggapony0925 <ninjeff06@gmail.com>
2023-10-18 15:25:31 -0400

    added page configuration

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
 import streamlit as st
+import config
+
+
+def main():
+
+    st.set_page_config(
+        page_title="Vending Machine calculation Application",
+        page_icon='💵',
+        layout='wide',
+        initial_sidebar_state="collapsed",
+        menu_items = {
+            "Get Help": "https://github.com/wiggapony0925/VendingMachineApplication",
+            "Report a bug": "https://github.com/wiggapony0925/VendingMachineApplication",
+            "About": "# this application's purpose is to calculate expenses with in your buisness to be organized."
+        }
+
+    )
